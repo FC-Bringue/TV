0bfa15e10935aa025fbdeda18da6720fc77134b7
Adrien Albuquerque <adrialb95@gmail.com>
2022-04-06 00:25:25 +0200

    Footer
    Une première partie du footer est fait. Il faut encore ajuster le css

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import MusicPlayer from './MusicPlayer';
 
 
 function Footer() {
   return (
-    <div className='MusicPlayerContainer'>
+    <>
         <MusicPlayer/>
-    </div>
+    </>
   );
 }
 
