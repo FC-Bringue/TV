f891e7c7b453898c922a6ffd86e91708602a8565
lucalg322 <95612509+lucalg322@users.noreply.github.com>
2022-04-07 14:22:55 +0200

    header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../style/header.css';
 
 
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light ">
   <div className="container-fluid justify-content-start">
-    <a className="navbar-brand" href="#"> LOGO  </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <h3>LE MACUMBA</h3>
-    <a className="navbar-brand ms-auto" href="#"> QRCode  </a>
+  <img className='logo' src='/Images/logo.png' alt='logo'/>
+    <h3 className='title'>LE MACUMBA</h3>
+    <img className='qrCode ms-auto' src='/Images/qrcode.png' alt='Qr Code'/>
     
     
   </div>
