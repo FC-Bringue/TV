726ca340d2d8b96b19cf827f58c734f5954a80ff
Adrien Albuquerque <adrialb95@gmail.com>
2022-04-07 14:29:17 +0200

    linear gradient

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-lights">
   <div className="container-fluid justify-content-start">
     <a className="navbar-brand" href="#"> LOGO  </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
